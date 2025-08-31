4b777d99e22607ae8be2b974b7c39b70469c3e59
Azizmatov Sharofiddin <azizmatov1202@gmail.com>
2025-08-31 18:20:46 +0300

    refactor: comment out FeaturedCars component in HomePage for future implementation

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default async function HomePage() {
     <div className="min-h-screen">
       <HeroSection />
       <SearchBar />
-      <FeaturedCars cars={(featuredCars as any) || []} />
+      {/* <FeaturedCars cars={(featuredCars as any) || []} /> */}
       <HowItWorks />
     </div>
   );
