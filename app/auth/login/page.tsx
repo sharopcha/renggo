0f22dcd746c3fb99f53b9aa844b6b45cd10e9be0
Azizmatov Sharofiddin <azizmatov1202@gmail.com>
2025-09-10 02:16:08 +0300

    feat: update secondary color for improved contrast in login page and global styles

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-secondary">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
