1317c01bcb49a216acf3608486d3c0f0f3571413
Azizmatov Sharofiddin <azizmatov1202@gmail.com>
2025-09-09 22:43:20 +0300

    feat: implement user logout functionality in NavUser component and redirect logged-in users from login page to backoffice

--- components/layout/nav-user.tsx
+++ components/layout/nav-user.tsx
@@ -25,6 +25,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { createClient } from "@/lib/supabase/client";
 
 export function NavUser({
   user,
@@ -36,6 +37,11 @@ export function NavUser({
   };
 }) {
   const { isMobile } = useSidebar();
+  const supabase = createClient();
+
+  const onLogout = async () => {
+    await supabase.auth.signOut();
+  }
 
   return (
     <SidebarMenu>
@@ -98,7 +104,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={onLogout}>
               <LogOut />
               Log out
             </DropdownMenuItem>
